2da906a29ef489f20f788e96d8f5c6a1ef99d029
vincent <vtixier1@me.com>
2023-11-25 14:03:39 -0500

    Exercise 4 : Guess status.

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -6,7 +6,7 @@ import GuessInput from "../GuessInput";
 import GuessResults from "../GuessResults";
 
 // Pick a random word on every pageload.
-const answer = sample(WORDS);
+export const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { range } from "../../utils";
 import { WORD_LENGTH } from "../../constants";
+import { checkGuess } from "../../game-helpers";
+import { answer } from "../Game/Game";
 
 function Guess({ value }) {
+  const wordStatus = value && checkGuess(value, answer);
+
+  const classNames = range(WORD_LENGTH).map((_, index) => {
+    const letterStatus = wordStatus?.at(index)?.status;
+
+    if (!letterStatus) {
+      return "cell";
+    }
+    return `cell ${letterStatus}`;
+  });
+
   return (
     <p className="guess">
-      {range(WORD_LENGTH).map((_, index) => (
-        <span key={index} className="cell">
+      {classNames.map((_, index) => (
+        <span key={index} className={classNames[index]}>
           {value?.at(index)}
         </span>
       ))}
